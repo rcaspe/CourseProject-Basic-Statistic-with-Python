deac20cf6eb484e6837bb4249bb84565fd2e137b
rhcgc <rcgcaspe221294@gmail.com>
2022-11-29 10:14:59 +0800

    progress jupyter notebook

---
+++ setup-environment.py
@@ -0,0 +1,17 @@
+
+# type miniconda
+# download latest python version
+# install conda
+
+# to check if properly installed
+# cmd
+    # conda --version
+    # python --version
+        # if doesn't shows the version re install the application
+
+# create virtual environtment
+
+# cmd
+    # conda create -n myenv pandas numpy matploylib
+        # syntaxt
+            # conda create -n > (name of environment) > (packages (optional)) > (libraries (optional))
